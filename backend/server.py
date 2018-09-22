5b61affb9cb55f894107f692ac612b8ab7414543
William <wliu2@andrew.cmu.edu>
2018-09-22 01:59:57 -0400

    server can now take image posts

--- backend/server.py
+++ backend/server.py
@@ -1,29 +1,34 @@
-from BaseHTTPServer import HTTPServer
-from BaseHTTPServer import BaseHTTPRequestHandler
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
 import cgi
 from tempfile import SpooledTemporaryFile
-import SimpleHTTPServer
-import SocketServer
 from PIL import Image
 
 class PostHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        length = 5000 #not sure what this should be
+        print("got request")
+        length = int(self.headers.get('content-length'))
         with SpooledTemporaryFile() as imgfile:
             read = 0
+            i = 0
             while read < length:
-                buffer = self.rfile.read(1024)
+                buffer = self.rfile.read(min(1024, length-read))
                 if not buffer:
-                # too short, return error
+                    # too short, return error
+                    print("BAD SHORT BUFFER SHIT")
+                    return
                 imgfile.write(buffer)
                 read += len(buffer)
-            if read > length or self.rfile.read(1):
-                print("POST TOO LONG")
-
-            img_file.seek(0)
-            img = Image.open(img_file)
+                i+=1
+            imgfile.seek(0)
+            img = Image.open(imgfile)
             newIm = img.convert('RGB')
             newIm.save("poster.jpg")
+        print("done with post")
+        message = "Successfully received image"
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(str.encode(message))
         return
 
 server = HTTPServer(('localhost', 8080), PostHandler)
